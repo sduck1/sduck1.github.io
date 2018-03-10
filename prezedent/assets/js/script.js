f7a5ff835fc771811ab3e8b009d586ed43506a2d
sduck1 <33058212+sduck1@users.noreply.github.com>
2018-03-10 19:35:40 +0300

    test

--- prezedent/assets/js/script.js
+++ prezedent/assets/js/script.js
@@ -6,3 +6,61 @@ window.addEventListener("load", function () {
     });
 
 });
+
+function showTime(id) {
+    let modal = document.getElementById('myModal');
+    let btn = document.getElementById(id);
+    let span = document.getElementsByClassName('close2')[0];
+
+
+    modal.style.display = 'block';
+
+    span.onclick = function () {
+        modal.style.display = 'none';
+    };
+
+    window.onclick = function (event) {
+        if(event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+}
+
+
+// function showTime2(id) {
+//     let modal = document.getElementById('myModal2');
+//     let btn = document.getElementById(id);
+//     let span = document.getElementsByClassName('close2')[0];
+//
+//
+//     modal.style.display = 'block';
+//
+//     span.onclick = function () {
+//         modal.style.display = 'none';
+//     };
+//
+//     window.onclick = function (event) {
+//         if(event.target == modal) {
+//             modal.style.display = 'none';
+//         }
+//     }
+// }
+
+function showTime3(id) {
+    let modal = document.getElementById('myModal3');
+    let btn = document.getElementById(id);
+    let span = document.getElementsByClassName('close3')[0];
+
+
+    modal.style.display = 'block';
+
+    span.onclick = function () {
+        modal.style.display = 'none';
+    };
+
+    window.onclick = function (event) {
+        if(event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+}
