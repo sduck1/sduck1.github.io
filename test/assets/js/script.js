185d6972e763d6fc4958ddce4928cad243b2c372
sduck1 <33058212+sduck1@users.noreply.github.com>
2018-03-12 20:19:21 +0300

    test

---
+++ test/assets/js/script.js
@@ -0,0 +1,4 @@
+$('.single-block').slick({
+    prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><img src="assets/img/previous.png" alt=""></button>',
+    nextArrow: '<button id="next" type="button" class="btn btn-juliet"><img src="assets/img/next.png" alt=""></button>'
+});
